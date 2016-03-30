8aa02f64c3ae437e9699b1a72a9610a9b6816b47
Sage Arbor <sagearbor@gmail.com>
2016-03-30 13:28:34 -0400

    add mongoDB test DB and bring back restaurant data - just 3 variables - name, , .

--- server/app.js
+++ server/app.js
@@ -1,23 +1,38 @@
 var express = require('express');
 var notelyServerApp = express();
 
+var db = require('mongoose');
+db.connect('mongodb://localhost:27017/test');
+
+var TestSchema = db.Schema({
+  name: String
+});
+
+var Restaurant = db.model('Restaurant', TestSchema);
+
 // Cross-Origin Resource Sharing (CORS) middleware
-    notelyServerApp.use(function(req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
-      next();
-    });
+notelyServerApp.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 notelyServerApp.get('/', function(req, res) {
-  res.json([
-    {
-      title: 'Edited Hardcoded note',
-      body_html: 'Cool note. Aww, shucks.'
-    },
-    {
-      title: 'Another hardcoded note',
-      body_html: "Ain't life grand?"
-    }
-  ]);
+  // Restaurant.find().limit(3).then(function(restaurantData) {
+  //   res.json(restaurantData);
+    Restaurant.find({ }, { name: 1, cuisine: 1, borough: 1 } ).limit(30).then(function(restaurantData) {
+      res.json(restaurantData);
+  });
+
+  // res.json([
+  //   {
+  //     title: 'Edited hardcoded note!',
+  //     body_html: 'Cool note. Aww, shucks.'
+  //   },
+  //   {
+  //     title: 'Another edited hardcoded note',
+  //     body_html: "Ain't life grand?"
+  //   }
+  // ]);
 });
 
 notelyServerApp.listen(3030, function() {
