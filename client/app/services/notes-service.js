e1c5fe4220bf81842874b84b929534a51ab6a559
Sage Arbor <sagearbor@gmail.com>
2016-03-31 14:49:24 -0400

    Keep sidebar in sync after updating a note.

--- client/app/services/notes-service.js
+++ client/app/services/notes-service.js
@@ -32,7 +32,27 @@
       }).then(function(response) {
         _this.notes.unshift(response.data.note);
       });
-    }
+    };
+
+    _this.update = function(note) {
+      return $http.put('http://localhost:3030/notes/' + note._id, {
+        note: {
+          title: note.title,
+          body_html: note.body_html
+        }
+      }).then(function(response) {
+        _this.replaceNote(response.data.note);  // making this work
+      });
+    };
+
+    _this.replaceNote = function(updatedNote) {
+      for (var i = 0; i < _this.notes.length; i++) {
+        if (_this.notes[i]._id === updatedNote._id) {
+          _this.notes[i] = updatedNote;
+          return;
+        }
+      }
+    };
 
     _this.findById = function(noteId) {
       for (var i = 0; i < _this.notes.length; i++) {
@@ -43,6 +63,5 @@
       }
       return {};
     };
-
   }
 }());
