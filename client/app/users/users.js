3afa6b797f4fae171911a1f7329dc3f77379d9db
Sage Arbor <sagearbor@gmail.com>
2016-04-01 11:22:53 -0400

    Add signUp directive and template.

---
+++ client/app/users/users.js
@@ -0,0 +1,13 @@
+{
+  let usersConfig = function($stateProvider) {
+    $stateProvider
+      .state('sign-up', {
+        url: '/sign-up',
+        template: '<sign-up></sign-up>'
+      });
+  };
+  usersConfig.$inject = ['$stateProvider'];
+
+  angular.module('notely')
+    .config(usersConfig);
+}
